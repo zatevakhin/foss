b8e81563a44f127c3cdf40852c8c03fef9edc785
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2018-09-04 00:43:32 +0300

    Removed old shaders.

--- resources/shaders/trash/draw2D.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 color;
-
-uniform vec4 background;
-
-void main() {
-    color = background; //vec4(0.2f, 0.2f, 0.2f, 0.5f);
-}
--- resources/shaders/trash/lamp.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-out vec4 color;
-
-void main() {
-
-    // Set alle 4 vector values to 1.0f
-    color = vec4(1.0f);
-
-}
