2f69b6049825cfa155418e0470035d562dfe7951
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2021-07-09 23:14:21 +0300

    Added Procedural components.

---
+++ sources/app/components/CProceduralComponent.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "app/scene/IProceduralGenerator.hpp"
+#include "app/scene/IProceduralSettings.hpp"
+#include "ecs/Component.hpp"
+
+
+class CProceduralComponent : public ecs::Component<CProceduralComponent>
+{
+public:
+    CProceduralComponent(IProceduralGenerator* generator)
+        : mGenerator(generator)
+    {
+        mGenerator->generate();
+    }
+
+    TModelPtr get() const
+    {
+        return mGenerator->get();
+    }
+
+    EProceduralType getType() const
+    {
+        return mGenerator->getType();
+    }
+
+    const IProceduralSettings& getSettings() const
+    {
+        return mGenerator->getSettings();
+    }
+
+    void setSettings(IProceduralSettings& settings)
+    {
+        mGenerator->setSettings(settings);
+        mGenerator->generate();
+    }
+
+private:
+    TProceduralGenerator mGenerator;
+};
---
+++ sources/app/scene/IProceduralGenerator.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "EProceduralType.hpp"
+#include "IModel.hpp"
+#include "IProceduralSettings.hpp"
+
+class IProceduralGenerator
+{
+public:
+    virtual void setSettings(IProceduralSettings& settings) = 0;
+    virtual const IProceduralSettings& getSettings() const = 0;
+    virtual void generate() = 0;
+
+    virtual TModelPtr get() const = 0;
+    virtual EProceduralType getType() const = 0;
+
+    virtual ~IProceduralGenerator(){};
+};
+
+using TProceduralGenerator = std::unique_ptr<IProceduralGenerator>;
