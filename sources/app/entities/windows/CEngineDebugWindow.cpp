01d19ed430775ce4ed0854bfdfc1cfa6955ccc7b
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2021-07-09 23:27:46 +0300

    Returned ImGUI demo.

--- sources/app/entities/windows/CEngineDebugWindow.cpp
+++ sources/app/entities/windows/CEngineDebugWindow.cpp
@@ -15,6 +15,8 @@ CEngineDebugWindow::~CEngineDebugWindow()
 
 void CEngineDebugWindow::draw()
 {
+    ImGui::ShowDemoWindow();
+
     ImGui::Text("App %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
                 ImGui::GetIO().Framerate);
 
