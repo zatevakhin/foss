30c5a224c5e6c820c8f1cab96b7f3283731ef338
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2021-07-09 22:20:57 +0300

    Changes:
    - Added TNoiseSettings;
    - Refactored Noise class.

--- sources/app/math/noise.hpp
+++ sources/app/math/noise.hpp
@@ -49,220 +49,215 @@ class Noise
 
 private:
     std::array<int, randomSize * 2> mRandom;
-    double dot(const glm::ivec3& g, double x, double y, double z);
+    double dot(const glm::ivec3& g, double x, double y, double z)
+    {
+        return g.x * x + g.y * y + g.z * z;
+    }
 
 public:
-    Noise();
-    Noise(unsigned int seed);
-    ~Noise();
+    Noise()
+        : Noise(0)
+    {
+    }
 
-    void randomize(unsigned int seed);
-    float evaluate(glm::vec3 point);
+    Noise(unsigned int seed)
+    {
+        randomize(seed);
+    }
+
+    ~Noise() = default;
+
+    void randomize(unsigned int seed)
+    {
+        if (seed)
+        {
+            USeed u{seed};
+
+            for (int i = 0; i < source.size(); ++i)
+            {
+                mRandom[i] = source[i] ^ u.c[0];
+                mRandom[i] ^= u.c[1];
+                mRandom[i] ^= u.c[2];
+                mRandom[i] ^= u.c[3];
+
+                mRandom[i + randomSize] = mRandom[i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < randomSize; ++i)
+            {
+                mRandom[i + randomSize] = mRandom[i] = source[i];
+            }
+        }
+    }
+
+    float evaluate(glm::vec3 point)
+    {
+        double x = point.x;
+        double y = point.y;
+        double z = point.z;
+        double n0 = 0, n1 = 0, n2 = 0, n3 = 0;
+
+        // Noise contributions from the four corners
+        // Skew the input space to determine which simplex cell we're in
+        double s = (x + y + z) * F3;
+
+        int i = std::floor(x + s);
+        int j = std::floor(y + s);
+        int k = std::floor(z + s);
+
+        double t = (i + j + k) * G3;
+
+        // The x,y,z distances from the cell origin
+        double x0 = x - (i - t);
+        double y0 = y - (j - t);
+        double z0 = z - (k - t);
+
+        // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
+        // Determine which simplex we are in.
+        // Offsets for second corner of simplex in (i,j,k)
+        int i1, j1, k1;
+
+        // coords
+        int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
+
+        if (x0 >= y0)
+        {
+            if (y0 >= z0)
+            {
+                // X Y Z order
+                i1 = 1;
+                j1 = 0;
+                k1 = 0;
+                i2 = 1;
+                j2 = 1;
+                k2 = 0;
+            }
+            else if (x0 >= z0)
+            {
+                // X Z Y order
+                i1 = 1;
+                j1 = 0;
+                k1 = 0;
+                i2 = 1;
+                j2 = 0;
+                k2 = 1;
+            }
+            else
+            {
+                // Z X Y order
+                i1 = 0;
+                j1 = 0;
+                k1 = 1;
+                i2 = 1;
+                j2 = 0;
+                k2 = 1;
+            }
+        }
+        else
+        {
+            // x0 < y0
+            if (y0 < z0)
+            {
+                // Z Y X order
+                i1 = 0;
+                j1 = 0;
+                k1 = 1;
+                i2 = 0;
+                j2 = 1;
+                k2 = 1;
+            }
+            else if (x0 < z0)
+            {
+                // Y Z X order
+                i1 = 0;
+                j1 = 1;
+                k1 = 0;
+                i2 = 0;
+                j2 = 1;
+                k2 = 1;
+            }
+            else
+            {
+                // Y X Z order
+                i1 = 0;
+                j1 = 1;
+                k1 = 0;
+                i2 = 1;
+                j2 = 1;
+                k2 = 0;
+            }
+        }
+
+        // A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
+        // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z),
+        // and
+        // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z),
+        // where c = 1/6.
+
+        // Offsets for second corner in (x,y,z) coords
+        double x1 = x0 - i1 + G3;
+        double y1 = y0 - j1 + G3;
+        double z1 = z0 - k1 + G3;
+
+        // Offsets for third corner in (x,y,z)
+        double x2 = x0 - i2 + F3;
+        double y2 = y0 - j2 + F3;
+        double z2 = z0 - k2 + F3;
+
+        // Offsets for last corner in (x,y,z)
+        double x3 = x0 - 0.5;
+        double y3 = y0 - 0.5;
+        double z3 = z0 - 0.5;
+
+        // Work out the hashed gradient indices of the four simplex corners
+        int ii = i & 0xff;
+        int jj = j & 0xff;
+        int kk = k & 0xff;
+
+        // Calculate the contribution from the four corners
+        double t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
+        if (t0 > 0)
+        {
+            t0 *= t0;
+            int gi0 = mRandom[ii + mRandom[jj + mRandom[kk]]] % 12;
+            n0 = t0 * t0 * dot(Grad3[gi0], x0, y0, z0);
+        }
+
+        double t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1;
+        if (t1 > 0)
+        {
+            t1 *= t1;
+            int gi1 = mRandom[ii + i1 + mRandom[jj + j1 + mRandom[kk + k1]]] % 12;
+            n1 = t1 * t1 * dot(Grad3[gi1], x1, y1, z1);
+        }
+
+        double t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2;
+        if (t2 > 0)
+        {
+            t2 *= t2;
+            int gi2 = mRandom[ii + i2 + mRandom[jj + j2 + mRandom[kk + k2]]] % 12;
+            n2 = t2 * t2 * dot(Grad3[gi2], x2, y2, z2);
+        }
+
+        double t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3;
+        if (t3 > 0)
+        {
+            t3 *= t3;
+            int gi3 = mRandom[ii + 1 + mRandom[jj + 1 + mRandom[kk + 1]]] % 12;
+            n3 = t3 * t3 * dot(Grad3[gi3], x3, y3, z3);
+        }
+
+        // Add contributions from each corner to get the final noise value.
+        // The result is scaled to stay just inside [-1,1]
+        return (n0 + n1 + n2 + n3) * 32;
+    }
 };
 
-Noise::Noise()
+struct TNoiseSettings
 {
-    randomize(0U);
-}
-
-Noise::Noise(unsigned int seed)
-{
-    randomize(seed);
-}
-
-Noise::~Noise()
-{
-}
-
-
-void Noise::randomize(unsigned int seed)
-{
-    if (seed)
-    {
-        USeed u{seed};
-
-        for (int i = 0; i < source.size(); ++i)
-        {
-            mRandom[i] = source[i] ^ u.c[0];
-            mRandom[i] ^= u.c[1];
-            mRandom[i] ^= u.c[2];
-            mRandom[i] ^= u.c[3];
-
-            mRandom[i + randomSize] = mRandom[i];
-        }
-    }
-    else
-    {
-        for (int i = 0; i < randomSize; ++i)
-        {
-            mRandom[i + randomSize] = mRandom[i] = source[i];
-        }
-    }
-}
-
-float Noise::evaluate(glm::vec3 point)
-{
-    double x = point.x;
-    double y = point.y;
-    double z = point.z;
-    double n0 = 0, n1 = 0, n2 = 0, n3 = 0;
-
-    // Noise contributions from the four corners
-    // Skew the input space to determine which simplex cell we're in
-    double s = (x + y + z) * F3;
-
-    int i = std::floor(x + s);
-    int j = std::floor(y + s);
-    int k = std::floor(z + s);
-
-    double t = (i + j + k) * G3;
-
-    // The x,y,z distances from the cell origin
-    double x0 = x - (i - t);
-    double y0 = y - (j - t);
-    double z0 = z - (k - t);
-
-    // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
-    // Determine which simplex we are in.
-    // Offsets for second corner of simplex in (i,j,k)
-    int i1, j1, k1;
-
-    // coords
-    int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
-
-    if (x0 >= y0)
-    {
-        if (y0 >= z0)
-        {
-            // X Y Z order
-            i1 = 1;
-            j1 = 0;
-            k1 = 0;
-            i2 = 1;
-            j2 = 1;
-            k2 = 0;
-        }
-        else if (x0 >= z0)
-        {
-            // X Z Y order
-            i1 = 1;
-            j1 = 0;
-            k1 = 0;
-            i2 = 1;
-            j2 = 0;
-            k2 = 1;
-        }
-        else
-        {
-            // Z X Y order
-            i1 = 0;
-            j1 = 0;
-            k1 = 1;
-            i2 = 1;
-            j2 = 0;
-            k2 = 1;
-        }
-    }
-    else
-    {
-        // x0 < y0
-        if (y0 < z0)
-        {
-            // Z Y X order
-            i1 = 0;
-            j1 = 0;
-            k1 = 1;
-            i2 = 0;
-            j2 = 1;
-            k2 = 1;
-        }
-        else if (x0 < z0)
-        {
-            // Y Z X order
-            i1 = 0;
-            j1 = 1;
-            k1 = 0;
-            i2 = 0;
-            j2 = 1;
-            k2 = 1;
-        }
-        else
-        {
-            // Y X Z order
-            i1 = 0;
-            j1 = 1;
-            k1 = 0;
-            i2 = 1;
-            j2 = 1;
-            k2 = 0;
-        }
-    }
-
-    // A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
-    // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z),
-    // and
-    // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z),
-    // where c = 1/6.
-
-    // Offsets for second corner in (x,y,z) coords
-    double x1 = x0 - i1 + G3;
-    double y1 = y0 - j1 + G3;
-    double z1 = z0 - k1 + G3;
-
-    // Offsets for third corner in (x,y,z)
-    double x2 = x0 - i2 + F3;
-    double y2 = y0 - j2 + F3;
-    double z2 = z0 - k2 + F3;
-
-    // Offsets for last corner in (x,y,z)
-    double x3 = x0 - 0.5;
-    double y3 = y0 - 0.5;
-    double z3 = z0 - 0.5;
-
-    // Work out the hashed gradient indices of the four simplex corners
-    int ii = i & 0xff;
-    int jj = j & 0xff;
-    int kk = k & 0xff;
-
-    // Calculate the contribution from the four corners
-    double t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
-    if (t0 > 0)
-    {
-        t0 *= t0;
-        int gi0 = mRandom[ii + mRandom[jj + mRandom[kk]]] % 12;
-        n0 = t0 * t0 * dot(Grad3[gi0], x0, y0, z0);
-    }
-
-    double t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1;
-    if (t1 > 0)
-    {
-        t1 *= t1;
-        int gi1 = mRandom[ii + i1 + mRandom[jj + j1 + mRandom[kk + k1]]] % 12;
-        n1 = t1 * t1 * dot(Grad3[gi1], x1, y1, z1);
-    }
-
-    double t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2;
-    if (t2 > 0)
-    {
-        t2 *= t2;
-        int gi2 = mRandom[ii + i2 + mRandom[jj + j2 + mRandom[kk + k2]]] % 12;
-        n2 = t2 * t2 * dot(Grad3[gi2], x2, y2, z2);
-    }
-
-    double t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3;
-    if (t3 > 0)
-    {
-        t3 *= t3;
-        int gi3 = mRandom[ii + 1 + mRandom[jj + 1 + mRandom[kk + 1]]] % 12;
-        n3 = t3 * t3 * dot(Grad3[gi3], x3, y3, z3);
-    }
-
-    // Add contributions from each corner to get the final noise value.
-    // The result is scaled to stay just inside [-1,1]
-    return (n0 + n1 + n2 + n3) * 32;
-}
-
-double Noise::dot(const glm::ivec3& g, double x, double y, double z)
-{
-    return g.x * x + g.y * y + g.z * z;
-}
+    float strength{1.f};
+    float roughness{1.f};
+    glm::vec3 center{0.f};
+};
